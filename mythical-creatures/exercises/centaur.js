98b86fb962a23244381dd748647eb83ddff6fb7f
Kristi Miller <kristi.ann.miller@gmail.com>
2021-02-21 10:04:48 -0700

    Add increaseAction method

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -4,11 +4,20 @@ class Centaur {
     this.breed = data.type
     this.cranky = false
     this.standing = true
+    this.actionCount = 0
+  }
+  increaseAction() {
+    this.actionCount++
+    if(this.actionCount > 2) {
+      this.cranky = true
+    }
   }
   shoot() {
+    this.increaseAction()
     return 'Twang!!!'
   }
   run() {
+    this.increaseAction()
     return 'Clop clop clop clop!!!'
   }
 }
