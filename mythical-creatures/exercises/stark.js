c9ccd84b20ca982dcd92922d81cd6312da604619
Kristi Miller <kristi.ann.miller@gmail.com>
2021-02-22 14:29:36 -0700

    Add safe to constructor and houseWords method

--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -2,6 +2,10 @@ class Stark {
   constructor(name, location) {
     this.name = name
     this.location = location || 'Winterfell'
+    this.safe = false
+  }
+  houseWords() {
+    return 'Winter is Coming'
   }
 }
 
