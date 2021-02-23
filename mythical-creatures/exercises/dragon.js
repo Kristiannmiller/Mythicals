de977caf1f8937d1ece4bf8dee16c89c675f641c
Kristi Miller <kristi.ann.miller@gmail.com>
2021-02-23 08:57:32 -0700

    Add eat method

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -4,6 +4,13 @@ class Dragon {
     this.rider = rider
     this.color = color
     this.hungry = true
+    this.stomach = 0
+  }
+  eat() {
+    this.stomach++
+    if(this.stomach > 2) {
+      this.hungry = false
+    }
   }
 }
 
