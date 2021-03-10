421523838b4fcae42e5c501e4915bde4e8db7550
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-10 09:44:36 -0700

    Add performShowStopper method

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -13,5 +13,8 @@ class Magician {
     if(this.practice > 2) this.confident = true
     return this.topHat ? 'PULL RABBIT FROM TOP HAT' : 'PULL DOVE FROM SLEEVE'
   }
+  performShowStopper() {
+    return this.confident ? 'WOW! The magician totally just sawed that person in half!' : 'Oh no! Practice more before attempting this trick!'
+  }
 }
 module.exports = Magician
