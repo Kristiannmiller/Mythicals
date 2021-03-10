1d4366b733e496f2f5ca61900fd58daa20fbc56a
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-10 09:43:24 -0700

    Add conditional for confidence in cast method

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -3,11 +3,14 @@ class Magician {
     this.name = name
     this.topHat = topHat
     this.confident = false
+    this.practice = 0
   }
   incantation(phrase) {
     return `${phrase.toUpperCase()}!`
   }
   cast() {
+    this.practice++
+    if(this.practice > 2) this.confident = true
     return this.topHat ? 'PULL RABBIT FROM TOP HAT' : 'PULL DOVE FROM SLEEVE'
   }
 }
