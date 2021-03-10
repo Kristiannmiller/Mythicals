959e381a36bfe96eb02120717fc30f8e492afba2
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-10 09:37:19 -0700

    Add incantation method

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -2,7 +2,9 @@ class Magician {
   constructor({name, topHat = true}) {
     this.name = name
     this.topHat = topHat
-
+  }
+  incantation(phrase) {
+    return `${phrase.toUpperCase()}!`
   }
 }
 module.exports = Magician
