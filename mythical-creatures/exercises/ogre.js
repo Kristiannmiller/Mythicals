b587ae8adad6cae27bcc2188d8334a32efb157d5
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-12 10:11:28 -0700

    Add swingAt method

--- mythical-creatures/exercises/ogre.js
+++ mythical-creatures/exercises/ogre.js
@@ -2,9 +2,13 @@ class Ogre {
   constructor(name, home) {
     this.name = name
     this.home = home || 'Swamp'
+    this.swings = 0
   }
   encounter(human) {
     human.encounterCounter++
+  }
+  swingAt(human) {
+    this.swings++
   }
 }
 module.exports = Ogre
